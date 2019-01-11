d95538b7a658f9ea044aa9e41545c3f4ef11e759
Raphael Camargo <raphael2.estagio@ibge.gov.br>
2019-01-11 11:40:49 -0200

    corrigindo de info-peso para info-nome no form.js

--- js/form.js
+++ js/form.js
@@ -1,4 +1,34 @@
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adiciona");
+
+    var paciente = obtemPacienteDoFormulario(form);
+
+    var pacienteTr = montaTr(paciente);
+
+    var erros = validaPaciente(paciente);
+
+    if (erros.length > 0) {
+        exibeMensagensDeErro(erros);
+
+        return;
+    }
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+
+    form.reset();
+
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
+
+});
+
 function obtemPacienteDoFormulario(form) {
+
     var paciente = {
         nome: form.nome.value,
         peso: form.peso.value,
@@ -6,7 +36,29 @@ function obtemPacienteDoFormulario(form) {
         gordura: form.gordura.value,
         imc: calculaImc(form.peso.value, form.altura.value)
     }
+
     return paciente;
+}
+
+function montaTr(paciente){
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
+    
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+   
+    return pacienteTr;
+}
+
+function montaTd(dado, classe) {
+    var td = document.createElement("td");
+    td.classList.add(classe);
+    td.textContent = dado;
+
+    return td;
 }
 
 function validaPaciente(paciente) {
@@ -50,46 +102,3 @@ function exibeMensagensDeErro(erros) {
         ul.appendChild(li);
     });
 }
-
-function montaTd(dado, classe) {
-    //Cria TD
-    var td = document.createElement("td");
-    td.classList.add(classe);
-    td.textContent = dado;
-
-    return td;
-}
-
-function montaTr(paciente){
-    //Cria TR
-    var pacienteTr = document.createElement("tr");
-    pacienteTr.classList.add("paciente");
-    //Cria TD's e adiciona dentro da TR
-    pacienteTr.appendChild(montaTd(paciente.nome, "info-peso"));
-    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
-    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
-    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
-    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
-    //retorna pacienteTr
-    return pacienteTr;
-}
-
-var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function(event) {
-    event.preventDefault();
-    var form = document.querySelector("#form-adiciona");
-    var paciente = obtemPacienteDoFormulario(form);
-    var pacienteTr = montaTr(paciente);
-    var erros = validaPaciente(paciente);
-
-    if (erros.length > 0) {
-        exibeMensagensDeErro(erros);
-        return;
-    }
-    var tabela = document.querySelector("#tabela-pacientes");
-    tabela.appendChild(pacienteTr);
-    form.reset();
-
-    var mensagensErro = document.querySelector("#mensagens-erro");
-    mensagensErro.innerHTML = "";
-});
--- js/remove-paciente.js
+++ js/remove-paciente.js
@@ -1,11 +1,12 @@
+var pacientes = document.querySelectorAll(".paciente");
+
 var tabela = document.querySelector("#tabela-pacientes");
 
 tabela.addEventListener("dblclick", function(event) {
     event.target.parentNode.classList.add("fadeOut");
     
-    if(event.target.tagName == 'TD'){// Somente executará nosso código caso o elemento em que clicamos seja um <td>
         setTimeout(function() {
             event.target.parentNode.remove();
         }, 500);
-    }
+
 });
